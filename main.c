234fe8633cf363fa9d8eeedbb8d76a36339e639b
Juwayyed <Juwayyed@gmail.com>
2026-05-15 19:43:53 +0200

    Removed quotes from switch conditions

--- main.c
+++ main.c
@@ -23,11 +23,41 @@ int main()
     /* Populating The Matrix With Dots */
     for(i = 0 ; i < N ; i++) {
         for(j = 0 ; j < N ; j++) {
-            if(i == 0 && j == 0 && ch == 'x') {
-                matx[j][i] = letters[0];
-            } else {    //Gen random num here then type the letters sequentially
-                matx[j][i] = '.';
+            if(ch == 'x') {
+                if(i == 0 && j == 0) {
+                    matx[j][i] = letters[0];
+                } else {    //Gen random num here then type the letters sequentially
+                    //Populate all other positions with dots
+                    matx[j][i] = '.';
+                    /* Generating A Random Number Between 0 and 3 */
+                    srand((unsigned)time(NULL));
+                    movement = rand() % NUM_DIRECTIONS;
+                    //printf("%d\n", movement);
+                    k++;
+                    switch(movement) {
+                    case 0:
+                    i > 0 ? matx[j][i-1] = letters[k] : exit;
+                    printf("%c", letters[k]);
+                    break;
+                    case 1:
+                    j > 0 ? matx[j-1][i] = letters[k] : exit;
+                    matx[j][i] = letters[k];
+                    printf("%c", letters[k]);
+                    break;
+                    case 2:
+                    j < 11 ? matx[j+1][i] = letters[k] : exit;
+                    matx[j][i] = letters[k];
+                    printf("%c", letters[k]);
+                    break;
+                    case 3:
+                    i < 11 ? matx[j][i+1] = letters[k] : exit;
+                    matx[j][i] = letters[k];
+                    printf("%c", letters[k]);
+                    break;
+                  }
+                }
             }
+
             printf("%c", matx[j][i]);
         }
         printf("\n");
@@ -36,10 +66,7 @@ int main()
 
     // Directions UP & DOWN = First Bracket || Left & Right = Second Bracket
 
-/* Generating A Random Number Between 0 and 3 */
-srand((unsigned)time(NULL));
-movement = rand() % NUM_DIRECTIONS;
-printf("%d\n", movement);
+
 /**************/
 
 /* Starting The Sequence */
@@ -48,24 +75,7 @@ printf("%d\n", movement);
 
 
                     if(i > 0 || j > 0) {
-                        switch(movement) {
-                            case '0':
-                            matx[j][i-1] = letters[k];
-                            printf("%c", letters[k]);
-                            break;
-                            case '1':
-                            matx[j-1][i] = letters[k];
-                            printf("%c", letters[k]);
-                            break;
-                            case '2':
-                            matx[j+1][i] = letters[k];
-                            printf("%c", letters[k]);
-                            break;
-                            case '3':
-                            matx[j][i+1] = letters[k];
-                            printf("%c", letters[k]);
-                            break;
-                        }
+
                         printf("%c", matx[j][i]);
                     }
                 }
